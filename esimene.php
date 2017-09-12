f2a9d2ee461c9c78230d46821d64b507a6383b2a
Kevin Perle <kevper@users.noreply.github.com>
2017-09-12 15:28:07 +0300

    Lisatud muutujad ja if lause

--- esimene.php
+++ esimene.php
@@ -1,3 +1,30 @@
+<?php
+	//kommentaar, paar muutujat järgmisena
+	$myName = "Kevin";
+	$MyFamilyName = "Perle";
+	//vaatame, mis kell on ja määrame päeva osa
+	$hournow = date("H");
+	//echo $hournow; 
+	$partofday = "";
+	if ( $hournow < 8){     // < > <= => !=
+	$partofday = "varajane hommik";
+	}
+	if ( $partofday >= 8 and $hournow < 16) {
+		$partofday = "koolipäev";
+	}
+	if ($hournow > 16){
+		$partofday = "vaba aeg";
+	}
+	//echo $partofday;
+	
+	//kui palju on koolipäeva lõpuni jäänud
+	$timenow = strtotime(date("d.m.Y H:i:s"));
+	//echo $timenow;
+	$schooldayend = strtotime(date("d.m.Y" ." " ."15:45"));
+	//echo $schooldayend;
+	$totheend = $schooldayend - $timenow;
+	echo (round($totheend / 60));
+?>
 <!DOCTYPE html>
 <html>
 <head>	
@@ -5,11 +32,17 @@
 	<title>Lehenimi</title>
 </head>
 <body>
-	<h1>Veebiprogremine</h1>
+<?php
+	//echo $myName ." " .$MyFamilyName;
+?>
+	<h1>veebiprogremine</h1>
 	<p>See õppetöö raames loodud leht vist ei sisalda tõsiseltvõetavat sisu</p>
 
 	<?php
-	echo "See on esimene jupp php abil väljastatud teksti";
+	echo "<p>See on esimene jupp php abil väljastatud teksti</p>";
+	echo "<p>Täna on ";
+	echo date("d.m.Y") .", kell lehe avamisel oli " .date("H:i:s");
+	echo", käes on " . $partofday.".</p>";
 	?>
 	
 	
