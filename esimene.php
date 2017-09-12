6ab525b4a42c352879069ccf00d8a4a88703c998
Kevin Perle <kevp@users.noreply.github.com>
2017-09-12 14:15:33 +0300

    Lisatud on esimene jupp php koodi

---
+++ esimene.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>	
+	<meta charset="utf-8">
+	<title>Lehenimi</title>
+</head>
+<body>
+	<h1>Veebiprogremine</h1>
+	<p>See õppetöö raames loodud leht vist ei sisalda tõsiseltvõetavat sisu</p>
+
+	<?php
+	echo "See on esimene jupp php abil väljastatud teksti";
+	?>
+	
+	
+	
+	
+	
+	
+	
+	
+	</body>
+
+
+
+
+
+
+
+
+
+</html>
